c317367fa1e7fbcb8d6352571f9e4b48fe7c0fb8
Dennis MacConnell <dennis.macconnell@oracle.com>
2025-09-28 19:33:03 -0400

    more variables

---
+++ variables/multiple_values.py
@@ -0,0 +1,24 @@
+# assign values to multiple variables in one line
+i,j,k = 1,2,3
+print(i)
+print(j)
+print(k)
+
+# assign values to multiple variables in one line
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+# One Value to Multiple Variables
+a = b = c = "cat"
+print(a)
+print(b)
+print(c)
+
+# Unpack a list:
+numbers = [10,20,30]
+i1, i2, i3 = numbers
+print(i1)
+print(i2)
+print(i3)
